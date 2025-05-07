18f872d5282b16f651ba44be02a59b292894f57a
f-kitayama <f-kitayama@sharing-tech.jp>
2025-05-07 15:06:20 +0900

    テーブル作成・編集

--- src/database/migrations/2014_10_12_000000_create_users_table.php
+++ src/database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,6 +15,7 @@ return new class extends Migration
             $table->id();
             $table->string('name');
             $table->string('email')->unique();
+            $table->string('icon')->nullable()->comment('アイコン画像のURL'); // デフォルト画像
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
